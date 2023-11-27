20ac5d1976623630c2962a626911361dd326dd31
jc11452 <jc11452@nyu.edu>
2023-11-27 10:52:15 +0400

    k

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const arrow = document.getElementById('arrow');
     const line = document.getElementById('line');
-  
+
     let isDragging = false;
 
     // Set the initial position of the arrow as a percentage of the screen width
@@ -13,43 +13,43 @@ document.addEventListener('DOMContentLoaded', function () {
       isDragging = true;
       arrow.style.cursor = 'grabbing';
     });
-  
+
     document.addEventListener('mousemove', function (e) {
       if (!isDragging) return;
-  
+
       const lineRect = line.getBoundingClientRect();
       let x = e.clientX - lineRect.left;
-  
+
       // Ensure the arrow stays within the bounds of the line
       if (x < 0) x = 0;
-      
 
-      
+
+
       if (x > lineRect.width-30) {
         // Redirect to "video.html" when the arrow reaches the right edge
         window.location.href = 'video.html';
         return;
       }
       // display each line of about video section based on the slider position
-      if (x > lineRect.width-605) {
-        document.getElementById('line1').style.opacity = 1;}
+      // if (x > lineRect.width-605) {
+      //   document.getElementById('line1').style.opacity = 1;}
+      //
+      // if (x > lineRect.width-405) {
+      //   document.getElementById('line2').style.opacity = 1;}
+      //
+      // if (x > lineRect.width-345) {
+      //   document.getElementById('line3').style.opacity = 1;}
 
-      if (x > lineRect.width-405) {
-        document.getElementById('line2').style.opacity = 1;}
-        
-      if (x > lineRect.width-345) {
-        document.getElementById('line3').style.opacity = 1;}
-    
-      if (x > lineRect.width-205) {
-        document.getElementById('line4').style.opacity = 1;}
-
-    
+      if (x > lineRect.width-350) {
+        document.getElementById('about').style.opacity = 1;}
 
 
-  
+
+
+
       arrow.style.left = `${lineRect.left + x}px`;
     });
-  
+
     document.addEventListener('mouseup', function () {
       isDragging = false;
       arrow.style.cursor = 'grab';
@@ -60,7 +60,6 @@ document.addEventListener('DOMContentLoaded', function () {
       const newArrowLeft = (window.innerWidth * initialArrowPosition) / 100;
       arrow.style.left = `${newArrowLeft}px`;
     });
-    
-    
-});
 
+
+});
